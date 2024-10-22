d51aa31a9ccc426b64c7b5f76212b0f581bdbc12
firhan14 <firhan@visinemapictures.com>
2024-10-22 20:45:42 +0700

    add middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import { PORT as port } from "./config"
 
+import errorMiddleware from "./middleware/error.middleware";
 import trackerRoute from "./routes/tracker.route";
 
 const PORT = Number(port) || 8000;
@@ -10,6 +11,7 @@ const app: Application = express();
 app.use(express.json());
 
 app.use("/api", trackerRoute);
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
